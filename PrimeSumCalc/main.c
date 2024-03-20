acc18cc905052142218d5433be06830e9a425c97
macius02 <mkuzmicki2002@gmail.com>
2024-03-20 11:46:27 +0100

    Finished

--- PrimeSumCalc/main.c
+++ PrimeSumCalc/main.c
@@ -8,106 +8,114 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
-#include <errno.h>
+#include <time.h>
+#include <sys/mman.h>
 
-void on_usr1(int signal) {
-    // Signal handler intentionally left blank
-}
+
+void on_usr1(int signal);
 
 int* generatePrimes(int range);
+
 void writePrimeNumbersToFile(int* primeNumbers, int size, char* path);
-void calculate_partial_sum(int id, int *primeNumbers, int* shared, int numberOfPrimes, int numberOfProcesses);
+
+void calculate_partial_sum(int id, int *primeNumbers, long long int* shared, int numberOfPrimes, int numberOfProcesses);
+
 
 int main(int argc, char** argv) {
     int numberOfProcesses = argc > 1 ? atoi(argv[1]) : 2;
     int numberOfPrimeNumbers = argc > 2 ? atoi(argv[2]) : 10;
     char *path = argc > 3 ? argv[3] : "primeNumbers.txt";
 
+	if(numberOfPrimeNumbers <= 0 || numberOfProcesses <= 0) {
+		printf("Illegal arguments");
+		return EXIT_FAILURE;
+
+	}
+
     if(numberOfProcesses > numberOfPrimeNumbers) {
         printf("Number of processes cannot be higher than the number of prime numbers\n");
         return EXIT_FAILURE;
     }
-
-    int *primeNumbers = generatePrimes(numberOfPrimeNumbers);
+	
+	int *primeNumbers = generatePrimes(numberOfPrimeNumbers);
     if(primeNumbers == NULL) {
-        printf("Failed to allocate memory\n");
-        return EXIT_FAILURE;
-    }
+		printf("Failed to allocate memory\n");
+		return EXIT_FAILURE;
+	}
     writePrimeNumbersToFile(primeNumbers, numberOfPrimeNumbers, path);
     sigset_t mask;
-    struct sigaction sa;
+
+    struct sigaction usr1;
     sigemptyset(&mask);
-    sa.sa_handler = (&on_usr1);
-    sa.sa_flags = 0;
-    sigaction(SIGUSR1, &sa, NULL);
+    usr1.sa_handler = (&on_usr1);
+    usr1.sa_mask = mask;
+    usr1.sa_flags = 0;
+    sigaction(SIGUSR1, &usr1, NULL);
+    int size = (numberOfProcesses+1) * sizeof(long long int);
+    
+    long long int *shared = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-    key_t key = ftok("primeNumbers.txt", 'R');
-    int size = numberOfProcesses * sizeof(int);
-    int shmid = shmget(key, size, 0666 | IPC_CREAT);
-    if(shmid == -1) {
-        printf("Error occurred while trying to create shared memory\n");
-        free(primeNumbers);
-        return EXIT_FAILURE;
+    if(shared == MAP_FAILED ) {
+		printf("Error occured while creating shared memory\n");
+		return EXIT_FAILURE;
     }
 
-    int *shared = (int *)shmat(shmid, NULL, 0);
-    if(shared == (void *)-1) {
-        printf("Error occurred while attaching memory segment to process\n");
-        shmctl(shmid, IPC_RMID, NULL);
-        return EXIT_FAILURE;
+	pid_t pid;
+    pid_t childPidNumbers[numberOfProcesses];
+    int index;
+
+    for(index = 0; index < numberOfProcesses; index++) {
+		pid = fork();
+		if(pid < 0) {
+			printf("Error occured while trying to fork\n");
+	        return EXIT_FAILURE;	
+		}
+		else if(pid == 0) {
+			pause();
+			break;
+		}
+		else childPidNumbers[index] = pid;
     }
-    int *shm;
-    pid_t childPids[numberOfProcesses];
-    for(int i = 0; i < numberOfProcesses; i++) {
-        pid_t pid = fork();
-        if(pid < 0) {
-            printf("Error occurred while trying to fork\n");
-            return EXIT_FAILURE;
-        } else if(pid == 0) { // Child process
-	    shmid = shmget(key, size, 0666);
-	    shm = shmat(key, size, IPC_CREAT | 0666);
-            pause(); // Wait for signal from parent
-            
-            shmdt(shared);
-            exit(EXIT_SUCCESS);
-        } else {
-            childPids[i] = pid;
-        }
-    }
-    if(pid == 0){
-	calculate_partial_sum(i, primeNumbers, shared, numberOfPrimeNumbers, numberOfProcesses);
+	
+
+    if(pid == 0) {
+    	calculate_partial_sum(index, primeNumbers, shared, numberOfPrimeNumbers, numberOfProcesses);
     }
     else {
-	for(int i = 0; i < numberOfProcesses; i++) {
-        	kill(childPids[i], SIGUSR1);
-    	}
-
+		clock_t start = clock();
     	for(int i = 0; i < numberOfProcesses; i++) {
-        	waitpid(childPids[i], NULL, 0); // Wait for all child processes to finish
-    	}
-	int totalSum = 0;
-    	for(int i = 0; i < numberOfProcesses; i++) {
-        	totalSum += shared[i];
-    	}
-    	printf("Total sum of prime numbers: %d\n", totalSum);
-    	}
-    
-    
+			kill(childPidNumbers[i], SIGUSR1);
 
-    
+		}
+		for(int i = 0; i < numberOfProcesses; i++) {
+			waitpid(childPidNumbers[i], NULL, 0);
+		}
+		clock_t end = clock();
+		double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		long long int totalSum = 0;
+		for(int i = 0; i < numberOfProcesses; i++) {
+			totalSum += shared[i];
+		}
+		printf("Total sum of prime numbers: %lld\n", totalSum);
+		printf("Child processes executed in: %f seconds\n", cpu_time_used);
 
-    shmdt(shared);
-    shmctl(shmid, IPC_RMID, NULL);
+		if (munmap(shared, size) == -1) {
+            printf("Failed to unmap");
+        }
+    }
+    
     free(primeNumbers);
 
     return EXIT_SUCCESS;
 }
 
+void on_usr1(int signal) {
+	//Used only to wake up the process
+}
 
-void calculate_partial_sum(int id, int *primeNumbers, int* shared, int numberOfPrimes, int numberOfProcesses){
+void calculate_partial_sum(int id, int *primeNumbers, long long int* shared, int numberOfPrimes, int numberOfProcesses){
 	int start = id * (numberOfPrimes/numberOfProcesses);
 	int end = (id+1) * (numberOfPrimes/numberOfProcesses);
-	printf("Start: %d, End: %d\n", start, end);
 	if(id == numberOfProcesses -1) {
 		end += numberOfPrimes % numberOfProcesses;
 	}
@@ -116,7 +124,6 @@ void calculate_partial_sum(int id, int *primeNumbers, int* shared, int numberOfP
 		sum += primeNumbers[i];
 	}
 	shared[id] = sum;
-	printf("output: %d\n", sum);
 }
 
 int* generatePrimes(int n) {
@@ -155,3 +162,4 @@ void writePrimeNumbersToFile(int *primeNumbers, int size, char *path) {
 	}
 	fclose(f);
 }
+
